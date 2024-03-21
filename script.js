98148165996a7ccfd309890cf11c70c5e31698a2
RodnelGAC <164328318+RodnelGAC@users.noreply.github.com>
2024-03-21 17:44:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,536 @@
+const question = [
+    //1
+    {
+        question: "The process by which businesses or other organizations develop international influence or start operating in international scale?",
+        answer: [
+            {
+                text: "JAMIE GUTTIEREZ – ANG (2018)", correct: true
+            },
+            {
+                text: "MANFRED STEGER (2014)", correct: false
+            },
+            {
+                text: "INDUSTRIAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+        ]
+    },
+    //2
+    {
+        question: "The expansion and intensification of social relations and consciousness across world time and world-space?",
+        answer: [
+            {
+                text: "JAMIE GUTTIEREZ – ANG (2018)", correct: false
+            },
+            {
+                text: "ECONOMIC", correct: false
+            },
+            {
+                text: "MANFRED STEGER (2014)", correct: true
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+        ]
+    },
+    //3
+    {
+        question: "what is TNC means?",
+        answer: [
+            {
+                text: "JAMIE GUTTIEREZ – ANG (2018)", correct: false
+            },
+            {
+                text: "multinational corporations", correct: false
+            },
+            {
+                text: "transnational corporation", correct: true
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+        ]
+    },
+    //4
+    {
+        question: "Particularly movement of material and goods between and within national boundaries.?",
+        answer: [
+            {
+                text: "INDUSTRIAL", correct: true
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "ECONOMIC", correct: false
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+        ]
+    },
+    //5
+    {
+        question: "Realization of a global common market, based on the freedom of exchange of goods and capital?",
+        answer: [
+            {
+                text: "INDUSTRIAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "ECONOMIC", correct: true
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+        ]
+    },
+    //6
+    {
+        question: "Realization of a global common market, based on the freedom of exchange of goods and capital?",
+        answer: [
+            {
+                text: "INDUSTRIAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "ECONOMIC", correct: true
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+        ]
+    },
+    //7
+    {
+        question: "Emergence of worldwide financial markets and better access to external financing for borrowers; emergence of under or un-regulated foreign exchange and speculative markets?",
+        answer: [
+            {
+                text: "INDUSTRIAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: true
+            },
+            {
+                text: "ECONOMIC", correct: false
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+        ]
+    },
+    //8
+    {
+        question: "which regulate the relationships among governments and guarantees the rights arising from social and economic globalization?",
+        answer: [
+            {
+                text: "INDUSTRIAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "ECONOMIC", correct: false
+            },
+            {
+                text: "POLITICAL", correct: true
+            },
+        ]
+    },
+    //9
+    {
+        question: "Survival in the new global business market calls for improved productivity and increased competition?",
+        answer: [
+            {
+                text: "INDUSTRIAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "COMPETITION", correct: true
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+        ]
+    },
+    //10
+    {
+        question: "Growth of cross-cultural contacts the desire to increase one's standard of living and enjoy foreign products and ideas?",
+        answer: [
+            {
+                text: "CULTURAL", correct: true
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "COMPETITION", correct: false
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+        ]
+    },
+    //11
+    {
+        question: "The creation of the international criminal court and international justice movements. Crime importation and raising awareness of global crime-fighting efforts and cooperation?",
+        answer: [
+            {
+                text: "CULTURAL", correct: false
+            },
+            {
+                text: "FINANCIAL", correct: false
+            },
+            {
+                text: "COMPETITION", correct: false
+            },
+            {
+                text: "LEGAL/ETHICAL", correct: true
+            },
+        ]
+    },
+    //12
+    {
+        question: "cross-boundary water air pollution over-fishing of the ocean spread of invasive species factories are built in developing countries with less environmental regulation which may increase pollution?",
+        answer: [
+            {
+                text: "CULTURAL", correct: false
+            },
+            {
+                text: "ECOLOGICAL", correct: true
+            },
+            {
+                text: "COMPETITION", correct: false
+            },
+            {
+                text: "LEGAL/ETHICAL", correct: false
+            },
+        ]
+    },
+    //13
+    {
+        question: "Increase circulation by people of all nations with fewer restrictions. Spreading of multiculturalism, and better individual access to cultural diversity through media?",
+        answer: [
+            {
+                text: "SOCIAL", correct: true
+            },
+            {
+                text: "ECOLOGICAL", correct: false
+            },
+            {
+                text: "COMPETITION", correct: false
+            },
+            {
+                text: "LEGAL/ETHICAL", correct: false
+            },
+        ]
+    },
+    //14
+    {
+        question: "Development of a global telecommunications infrastructure and greater data flow even into the world’s most remote areas?",
+        answer: [
+            {
+                text: "SOCIAL", correct: false
+            },
+            {
+                text: "ECOLOGICAL", correct: false
+            },
+            {
+                text: "TECHNICAL ", correct: true
+            },
+            {
+                text: "LEGAL/ETHICAL", correct: false
+            },
+        ]
+    },
+    //15
+    {
+        question: "What is ASEAN means?",
+        answer: [
+            {
+                text: "Association of South East Asian Nations", correct: true
+            },
+            {
+                text: "Asian  of South East Association Nations", correct: false
+            },
+            {
+                text: "Asian  of Southern East Association Nations ", correct: false
+            },
+            {
+                text: "Asian  of South Easter Association Nations", correct: false
+            },
+        ]
+    },
+    //16
+    {
+        question: "When Brunei Barussalam join the ASEAN?",
+        answer: [
+            {
+                text: "January 8, 1984", correct: true
+            },
+            {
+                text: "January 14, 1981", correct: false
+            },
+            {
+                text: "January 13, 1985 ", correct: false
+            },
+            {
+                text: "January 12, 1991", correct: false
+            },
+        ]
+    },
+    //17
+    {
+        question: "is the process of increasing the financial integration amongst countries. Consequently, economic globalization leads to the development of a global marketplace or a single world market?",
+        answer: [
+            {
+                text: "HISTORICAL", correct: false
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: true
+            },
+            {
+                text: "ECONOMY ", correct: false
+            },
+            {
+                text: "RESOURCES AND MARKETS", correct: false
+            },
+        ]
+    },
+    //18
+    {
+        question: "The trade routes were made over the years so that goods from  one kingdom or country moved to another. The well known silk-route from east to  west is an example of historical factor?",
+        answer: [
+            {
+                text: "HISTORICAL", correct: true
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: false
+            },
+            {
+                text: "ECONOMY ", correct: false
+            },
+            {
+                text: "RESOURCES AND MARKETS", correct: false
+            },
+        ]
+    },
+    //19
+    {
+        question: "The natural resources like minerals, coal, oil, gas, human resources, water, etc. make an important contribution in  globalisation?",
+        answer: [
+            {
+                text: "HISTORICAL", correct: false
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: false
+            },
+            {
+                text: "ECONOMY ", correct: false
+            },
+            {
+                text: "RESOURCES AND MARKETS", correct: true
+            },
+        ]
+    },
+     //20
+     {
+        question: "The cost of goods and values to the end user determine the movement of goods and value addition. The overall economics of a particular industry or trade is an important factor in globalization?",
+        answer: [
+            {
+                text: "HISTORICAL", correct: false
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: false
+            },
+            {
+                text: "ECONOMY ", correct: true
+            },
+            {
+                text: "RESOURCES AND MARKETS", correct: false
+            },
+        ]
+    },
+    //21
+    {
+        question: "The political issues of a country make globalisation channelised as per political bosses?",
+        answer: [
+            {
+                text: "HISTORICAL", correct: false
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: false
+            },
+            {
+                text: "POLITICAL", correct: true
+            },
+            {
+                text: "RESOURCES AND MARKETS", correct: false
+            },
+        ]
+    },
+    //22
+    {
+        question: "The technological development in the areas of production, product mix and firms are helping organisations to expand their operations. The hiring of services and procurement of sub-assemblies and components have a strong influence in the globalization process?",
+        answer: [
+            {
+                text: "INDUSTRIAL ORGANISATION", correct: true
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: false
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+            {
+                text: "RESOURCES AND MARKETS", correct: false
+            },
+        ]
+    },
+    //23
+    {
+        question: "Utilisation of built up capacities of production, sluggishness in domestic market and over production makes a manufacturing company look outward and go global?",
+        answer: [
+            {
+                text: "INDUSTRIAL ORGANISATION", correct: false
+            },
+            {
+                text: "ECONOMIC GLOBALIZATION", correct: false
+            },
+            {
+                text: "POLITICAL", correct: false
+            },
+            {
+                text: "PRODUCTION ISSUES", correct: true
+            },
+        ]
+    },
+    //24
+    {
+        question: "In January 1989, the U.N. Security Council passed a resolution to  implement in April the resolution to establish the U.N?",
+        answer: [
+            {
+                text: "Namibia", correct: true
+            },
+            {
+                text: "Japan", correct: false
+            },
+            {
+                text: "Central America", correct: false
+            },
+            {
+                text: "India", correct: false
+            },
+        ]
+    },
+    //25
+    {
+        question: "is a term coined to imply the economic liberalisation of previously command economies, which entails a broad re-structuring of world economic and political systems along western European and American lines. In this sense, globalisation is economic aid in exchange for the adoption of western political models and is thus a form of twenty-first century cultural imperialism. Globalisation is the core tactic used in areas of the world, such as the Middle East, that have hitherto evaded the ubiquitous western cultural umbrella?",
+        answer: [
+            {
+                text: "Internationalism", correct: false
+            },
+            {
+                text: "Globalisation", correct: true
+            },
+            {
+                text: "Administrative and financial reforms", correct: false
+            },
+            {
+                text: "Financial crisis", correct: false
+            },
+        ]
+    },
+];
+
+const questionElement = document.getElementById("question");
+const answerbutton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex= 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerbutton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerbutton.firstChild){
+        answerbutton.removeChild(answerbutton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorret = selectedBtn.dataset.correct === "true";
+    if(isCorret){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerbutton.children).forEach(button => {
+        if(button.dataset.correct == "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showCore(){
+    resetState();
+    questionElement.innerHTML = `Your Score ${score} out of ${question.length}!`;
+    nextButton.innerHTML = "Start Again ?";
+    nextButton.style.display = "block";
+
+}
+
+function  handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < question.length){
+        showQuestion();
+    }else{
+        showCore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < question.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
